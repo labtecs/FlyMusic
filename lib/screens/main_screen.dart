fc4e3027d0a8f1e2d0d00ff8125f204e4ed35fee
Labtecs <labtecs@vivaldi.net>
2019-12-12 23:27:23 +0100

    added drawer screen

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flymusic/screens/drawer_screen.dart';
 
 class StartScreen extends StatefulWidget {
   @override
@@ -9,7 +10,10 @@ class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      drawer: DrawerScreen(),
+      appBar: AppBar(
+        title: Text("Musik"),
+      ),
     );
   }
 }

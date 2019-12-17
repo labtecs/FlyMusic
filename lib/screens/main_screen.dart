091fe4418ca30845c760b057a25e6b5afa52219e
Nailik <kilianeller12@outlook.com>
2019-12-17 16:38:38 +0100

    folder dialog

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flymusic/screens/drawer_screen.dart';
 import 'package:flymusic/screens/track_list_screen.dart';
+import 'package:folder_picker/folder_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class StartScreen extends StatefulWidget {
   @override
@@ -8,6 +13,8 @@ class StartScreen extends StatefulWidget {
 }
 
 class _StartScreenState extends State<StartScreen> {
+  Directory externalDirectory;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +23,36 @@ class _StartScreenState extends State<StartScreen> {
         title: Text("FlyMusic - Tracks"),
       ),
       body: TrackList(),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            chooseFolder();
+          },
+          child: Icon(Icons.folder)),
     );
+  }
+
+  Future<void> chooseFolder() async {
+    var result =
+        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+    if (result[PermissionGroup.storage] == PermissionStatus.granted) {
+      await getStorage();
+
+      Navigator.of(context).push<FolderPickerPage>(
+          MaterialPageRoute(builder: (BuildContext context) {
+        return FolderPickerPage(
+            rootDirectory: Directory("/storage/emulated/0/"),
+            /// a [Directory] object
+            action: (BuildContext context, Directory folder) async {
+              print("Picked folder $folder");
+              Navigator.of(context).pop();
+            });
+      }));
+    }
+  }
+
+  Future<void> getStorage() async {
+    final directory =
+        await getExternalStorageDirectories(type: StorageDirectory.music);
+    setState(() => externalDirectory = directory[1]);
   }
 }

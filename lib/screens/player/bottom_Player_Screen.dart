f07341d13730224886813a8482f094ecaadfd9f5
Labtecs <labtecs@vivaldi.net>
2020-03-02 22:11:33 +0100

    bottom sheet fixed

--- lib/screens/player/bottom_Player_Screen.dart
+++ lib/screens/player/bottom_Player_Screen.dart
@@ -1,7 +1,9 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flymusic/music/music_queue.dart';
 import 'package:flymusic/screens/main_screen.dart';
+import 'package:flymusic/screens/player/player_screen.dart';
 
 
 class BottomPlayer extends StatefulWidget {
@@ -22,25 +24,31 @@ class _BottomPlayerState extends State<BottomPlayer> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              InkWell(
-                child: StartScreen.getArt3(MusicQueue.instance.currentSong, 10),
+              Container(
+                height: 60,
+                child: InkWell(
+                  child: StartScreen.getArt3(MusicQueue.instance.currentSong, 10),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => PlayerScreen()));
+                  },
+                ),
               ),
               IconButton(
                 icon: Icon(Icons.skip_previous),
                 onPressed: () {
-                  Fluttertoast.showToast(msg: "klick");
+                  previous();
                 },
               ),
               IconButton(
-                icon: Icon(Icons.play_arrow),
+                icon: Icon(getPlayIcon()),
                 onPressed: () {
-                  Fluttertoast.showToast(msg: "klick");
+                  play();
                 },
               ),
               IconButton(
                 icon: Icon(Icons.skip_next),
                 onPressed: () {
-                  Fluttertoast.showToast(msg: "klick");
+                  next();
                 },
               ),
             ],
@@ -48,6 +56,38 @@ class _BottomPlayerState extends State<BottomPlayer> {
       ),
     );
 
+  }
+
+  IconData getPlayIcon() {
+    if (MusicQueue.instance.audioPlayer.state == AudioPlayerState.PLAYING) {
+      return Icons.pause;
+    } else {
+      return Icons.play_arrow;
+    }
+  }
+
+  void play() async {
+    await MusicQueue.instance.playPause();
+    setState(() {
+      MusicQueue.instance.audioPlayer.state =
+          MusicQueue.instance.audioPlayer.state;
+    });
+  }
+
+  void next() async {
+    await MusicQueue.instance.playNext();
+    setState(() {
+      MusicQueue.instance.audioPlayer.state =
+          MusicQueue.instance.audioPlayer.state;
+    });
+  }
+
+  void previous() async {
+    await MusicQueue.instance.playPrevious();
+    setState(() {
+      MusicQueue.instance.audioPlayer.state =
+          MusicQueue.instance.audioPlayer.state;
+    });
   }
 
 

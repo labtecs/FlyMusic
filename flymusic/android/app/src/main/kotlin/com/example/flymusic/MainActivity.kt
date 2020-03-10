ed533e11788f47c8c7587610d1680f35d895ab7f
Nailik <kilianeller12@outlook.com>
2020-03-10 13:36:42 +0100

    recreate web

---
+++ flymusic/android/app/src/main/kotlin/com/example/flymusic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flymusic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- flymusic/lib/main.dart
+++ flymusic/lib/main.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flymusic/screens/main_screen.dart';
-import 'database/app_database.dart';
 
-AppDatabase database;
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  database = await $FloorAppDatabase
-      .databaseBuilder('app_database.db')
-      .build();
-  //AudioPlayer.logEnabled = true;
-  runApp(MyHomePage());
+void main() {
+  runApp(MyApp());
 }
 
-/*class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fly Music',
+      title: 'Flutter Demo',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: StartScreen(),
-
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}*/
-
+}
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -49,8 +47,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  _MyHomePageState(){
-  }
 
   void _incrementCounter() {
     setState(() {
@@ -78,24 +74,33 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: SizedBox(
-          width: 300, // 20%
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: TextEditingController(
-                    text: ""),
-                decoration: InputDecoration(hintText: 'Enter Hassio URL'),
-                onChanged: (value) {
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(hintText: 'Enter APi Key'),
-              ),
-            ],
-          ),
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(

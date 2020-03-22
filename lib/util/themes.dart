614ea4cbdc4e439f48491416599b00a8d7c5ceb6
Labtecs <labtecs@vivaldi.net>
2020-03-22 15:57:42 +0100

    Theme.dart

--- lib/util/themes.dart
+++ lib/util/themes.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 ThemeData basicTheme() {
-  TextTheme _basicTextTeme(TextTheme base) {
+  TextTheme _basicTextTheme(TextTheme base) {
     return base.copyWith(
       headline: base.headline.copyWith(
         fontFamily: 'MerriWeather',
         fontSize: 20.0,
         color: Colors.white,
-      )
+      ),
+      display1: base.title.copyWith(
+        fontFamily: 'asdf',
+        color: Colors.white,
+      ),
     );
   }
   final ThemeData base=ThemeData.light();
   return base.copyWith(
-    textTheme: _basicTextTeme(base.textTheme),
+    textTheme: _basicTextTheme(base.textTheme),
     primaryColor: Color.fromARGB(255, 47 , 114, 222),
   );
 }
